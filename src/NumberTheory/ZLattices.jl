708c0436f6e4fe5d29839c83b3676915c4287644
Kazak-11 <kazakov.artem2@yandex.ru>
2026-03-31 13:07:26 +0200

    snt

--- src/NumberTheory/ZLattices.jl
+++ src/NumberTheory/ZLattices.jl
@@ -74,7 +74,7 @@ function _get_edge_labeled_graph(char_vectors_set, gram_matrix)
 end
 
 function _lift_canonical_ordering(i, j, w, can_order) 
-    can_i = find(can_order .== (1+(w-1)*(i-1)))[0] # returnes index of min Si = (i,0) in canonical ordering 
+    can_i = find(can_order .== (1+(w-1)*(i-1)))[0] # returnes an index of min Si = (i,0) in the canonical ordering 
     can_j = find(can_order .== (1+(w-1)*(j-1)))[0]
     return can_i < can_j
 end
@@ -85,19 +85,18 @@ function _get_canonical_form(A, char_vectors_set, canonical_ordering)
     return U*A*transpose*U
 end
 
-
 function canonical_form(L::ZZLat)
     gram = gram_matrix(L)
     n = dim(ambient_space(L))
     if n>=2 && n<5
         char_vectors_set = characteristic_vectors(L)
-    elseif n>=5 #maybe it will be removed or condition made higher due to performance
+    elseif n>=5 #maybe it will be removed or this condition made higher due to performance
         char_vectors_set = _vor_vectors_set()
     end 
-    T1_graph = _get_edge_labeled_graph(char_vectors_set, gram) # transform from adjenctcy matrix A to edge-vertex weighted graph Ga, then to edge weighted graph T1(Ga)
-    T2_graph =  _edge_label_to_vertex_label(T1_graph, label = :weight) # transform from T1(Ga) to vertex weighted graph T2(T1(Ga))
+    T1_graph = _get_edge_labeled_graph(char_vectors_set, gram) # transform from the adjenctcy matrix A to the edge-vertex weighted graph Ga, then to the edge weighted graph T1(Ga)
+    T2_graph =  _edge_label_to_vertex_label(T1_graph, label = :weight) # transform from T1(Ga) to the vertex weighted graph T2(T1(Ga))
     p = length(char_vectors_set)
-    can_order = _canonical_perm(T2_graph, label = :weight) # get canonical ordering of T2(T1(Ga))
+    can_order = _canonical_perm(T2_graph, label = :weight) # get the canonical ordering of T2(T1(Ga))
     w = lastindex(can_order)/(p+2)
     lift_canonical_ordering = (i, j) => _lift_canonical_ordering(i, j, w, can_order)
     return _get_canonical_form(A, char_vectors_set, lift_canonical_ordering)
